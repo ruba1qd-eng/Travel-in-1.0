86cfe46d31284c805ac7ba50473df3e3e7edc62d
ruba1qd-eng <ruba1qd@gmail.com>
2026-09-02 16:54:57 +0300

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'l10n/app_localizations.dart';
+import 'state/app_state.dart';
+import 'theme/app_theme.dart';
+import 'screens/splash_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  runApp(TravelInApp(prefs: prefs));
+}
+
+class TravelInApp extends StatelessWidget {
+  final SharedPreferences prefs;
+  const TravelInApp({super.key, required this.prefs});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => AppState(prefs)..load(),
+      child: Consumer<AppState>(
+        builder: (context, state, _) {
+          return MaterialApp(
+            title: 'Travel In',
+            debugShowCheckedModeBanner: false,
+            theme: AppTheme.light,
+            darkTheme: AppTheme.dark,
+            themeMode: state.themeMode,
+            locale: state.locale,
+            supportedLocales: AppLocalizations.supportedLocales,
+            localizationsDelegates: const [
+              AppLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            builder: (context, child) {
+              // RTL/LTR كامل حسب اللغة
+              return Directionality(
+                textDirection: state.isRTL ? TextDirection.rtl : TextDirection.ltr,
+                child: child!,
+              );
+            },
+            home: const SplashScreen(),
+          );
+        },
+      ),
+    );
+  }
+}
