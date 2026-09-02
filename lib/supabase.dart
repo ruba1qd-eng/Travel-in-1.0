04acb78b403e6c394a10149b748b45fbc3d21544
ruba1qd-eng <ruba1qd@gmail.com>
2026-09-03 00:29:49 +0300

    Update supabase.dart

--- lib/supabase.dart
+++ lib/supabase.dart
@@ -2,10 +2,8 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> initSupabase() async {
   await Supabase.initialize(
-    url: const String.fromEnvironment('EXPO_PUBLIC_SUPABASE_URL', 
-      defaultValue: 'https://vtuuogjfmpknlnryxdvp.supabase.co'),
-    anonKey: const String.fromEnvironment('EXPO_PUBLIC_SUPABASE_ANON_KEY', 
-      defaultValue: 'sb_publishable_7EBiA6_yp_EWE92jV2HuvA_FFPozsDO'),
+    url: 'https://vtuuogjfmpknlnryxdvp.supabase.co',
+    anonKey: 'sb_publishable_7EBiA6_yp_EWE92jV2HuvA_FFPozsDO',
   );
 }
 
